7f8f82f0098d36e42b420482ac7cfc9f93ed9513
btwl <1253102853@qq.com>
2023-07-26 15:44:59 +0800

    update

---
+++ ic_sdk/help.md
@@ -0,0 +1,5 @@
+```sh
+cargo check &> check.txt
+```
+
+This will redirect both standard output and error output to the same file.  
--- ic_sdk/src/main.rs
+++ ic_sdk/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    print_test();
+}
+#[ic_cdk::query]
+fn print_test() {
+    ic_cdk::print("Hello World from DFINITY!");
 }
